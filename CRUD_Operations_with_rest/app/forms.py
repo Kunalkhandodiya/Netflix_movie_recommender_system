3e03be35f01b7a511688688aa7c5a1d89e58c6ed
Kunal khandodiya <123451489+Kunalkhandodiya@users.noreply.github.com>
2025-05-28 15:14:17 +0530

    Delete CRUD_Operations_with_rest directory

--- CRUD_Operations_with_rest/app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from app.models import Database
-
-class forms_Database(forms.ModelForm):
-    class Meta:
-        model=Database
-        fields="__all__"
--- CRUD_Operations_with_rest/app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from app.models import Database
-
-class Serializer_Database(serializers.ModelSerializer):
-    class Meta:
-        model = Database
-        fields="__all__"
